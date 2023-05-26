ef136b1f5b3a323060372ede64a6fb8faa5527f2
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-05-26 15:59:29 +0700

    DoubleLinkedList Revtraverse

--- Double_Linked_List/Double_Linked_List.cpp
+++ Double_Linked_List/Double_Linked_List.cpp
@@ -118,6 +118,24 @@ void DoubleLinkedList::traverse() {
     }
 }
 
+void DoubleLinkedList::revtraverse() {
+    if (listEmpty()) {
+        cout << "\nList is Empty" << endl;
+    }
+
+    else {
+        cout << "\nRecords in Descending order of roll number are : " << endl;
+        Node* currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while (currentNode != NULL) {
+            cout << currentNode->noMhs << " " << currentNode->name << endl;
+            currentNode = currentNode->prev;
+        }
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
